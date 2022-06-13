5d384773a3bc4493be934a74bf17b842d496a574
barnawi.amer <barnawi.amer@yahoo.com>
2022-06-13 17:01:03 +0200

    Exercise-2 solution

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -10,3 +10,21 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 ------------------------------------------------------------------------------*/
 
 // TODO add your JavaScript code here.
+
+const nickName = document.querySelector('#nickname');
+const favFood = document.querySelector('#fav-food');
+const homeTown = document.querySelector('#hometown');
+
+const myNickName = document.createTextNode('Amer Barnawi.');
+const myFavFood = document.createTextNode('Sheikh Al-Mahshi.');
+const myHomeTown = document.createTextNode('Safad.');
+
+nickName.parentNode.replaceChild(myNickName, nickName);
+favFood.parentNode.replaceChild(myFavFood, favFood);
+homeTown.parentNode.replaceChild(myHomeTown, homeTown);
+
+const ul = document.querySelectorAll('li');
+
+ul.forEach((li) => {
+  li.className = 'list-item';
+});

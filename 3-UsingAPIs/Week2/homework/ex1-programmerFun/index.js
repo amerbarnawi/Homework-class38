105746e905dcf633f7f36bec06359b04835ad1fa
barnawi.amer <barnawi.amer@yahoo.com>
2022-07-07 15:16:14 +0200

    Using APIs-Week.2-ex1-programmerFun-Solution.

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -18,28 +18,31 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  return fetch(url);
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
+  const image = document.createElement('img');
+  image.src = data.img;
+  document.body.appendChild(image);
   console.log(data);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
+  const paragraph = document.createElement('p');
+  paragraph.textContent = error;
+  document.body.appendChild(paragraph);
   console.log(error);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const promiseData = await requestData('https://xkcd.now.shx/?comic=latest');
+    const data = await promiseData.json();
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);

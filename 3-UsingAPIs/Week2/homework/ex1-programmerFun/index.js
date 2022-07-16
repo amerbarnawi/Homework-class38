471a0a9c357b2b5711f3715dab075dd35e058521
Amer <barnawi.amer@yahoo.com>
2022-07-16 14:51:35 +0200

    I changed the method of catching the error.

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -17,13 +17,18 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    url with `.shx`. There is no server at the modified url, therefore this 
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
-function requestData(url) {
-  return fetch(url);
+async function requestData(url) {
+    const response = await fetch(url);
+    if (response.ok){
+      const jsonData = await response.json();
+      return jsonData;
+    }
 }
 
 function renderImage(data) {
   const image = document.createElement('img');
   image.src = data.img;
+  image.alt = 'Comic image';
   document.body.appendChild(image);
   console.log(data);
 }
@@ -37,8 +42,7 @@ function renderError(error) {
 
 async function main() {
   try {
-    const promiseData = await requestData('https://xkcd.now.shx/?comic=latest');
-    const data = await promiseData.json();
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
     renderImage(data);
   } catch (error) {
     renderError(error);
